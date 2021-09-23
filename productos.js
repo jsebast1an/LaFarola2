404d2afc202de0153ee6f4413b0dbb7edd1888d1
jsebast1an <juanselopezg05@gmail.com>
2021-09-23 10:23:02 -0300

    imagenes dañadas

--- productos.js
+++ productos.js
@@ -1,7 +1,7 @@
 //todos los productos
 
 let stockProductos = [
-{cantidad:10, id:1, prenda: "Zapato", img:"imgs/adidas.jpg",marca: "Adidas", modelo: "Forum Mid Cream White", precio: 12000},
+{cantidad:10, id:1, prenda: "Zapato", img:"imgs/Adidas.jpg",marca: "Adidas", modelo: "Forum Mid Cream White", precio: 12000},
 {cantidad:10, id:2, prenda: "Zapato", img:"imgs/adidaslow.jpg",marca: "Adidas", modelo: "Forum Low Cream White", precio: 13000},   
 {cantidad:10, id:3, prenda: "Zapato", img:"imgs/adidasnegra.jpg",marca: "Adidas", modelo: "Forum 84 Mid Black", precio: 15000},
 {cantidad:10, id:4, prenda: "Zapato", img:"imgs/adidashigh.jpg",marca: "Adidas", modelo: "Forum Low Rose Orange", precio: 16900},
@@ -13,7 +13,7 @@ let stockProductos = [
 {cantidad:10, id:10, prenda: "Hoodie", img:"imgs/hoodieverde.jpg",marca: "La Farola", modelo: "Hoodie Azul Oscuro", precio: 3100},
 {cantidad:10, id:11, prenda: "Hoodie", img:"imgs/hoodieamarillo.jpg",marca: "La Farola", modelo: "Hoodie Amarillo Crop", precio: 2800},
 {cantidad:10, id:12, prenda: "Remera", img:"imgs/remeravino.jpg",marca: "La Farola", modelo: "Remera Vino", precio: 1900},
-{cantidad:10, id:13, prenda: "Remera", img:"imgs/remeraazul.jpg",marca: "La Farola", modelo: "Remera Azul", precio: 1900},   
+{cantidad:10, id:13, prenda: "Remera", img:"imgs/RemeraAzul.jpg",marca: "La Farola", modelo: "Remera Azul", precio: 1900},   
 {cantidad:10, id:14, prenda: "Remera", img:"imgs/remeranegra.jpg",marca: "La Farola", modelo: "Remera Negro", precio: 1900},]
  
 
